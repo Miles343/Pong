3c35330f951e2c46256bcec8785a1e870188ca0e
Miles343 <james.potter.nz@gmail.com>
2019-08-25 19:33:38 +1200

    Colour to window and diagram of classes.

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,16 +1,53 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class PongPanel extends JPanel implements ActionListener, KeyListener{
+	
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	
+	public PongPanel() {
+		setBackground(BACKGROUND_COLOUR);
+	}
+	@Override
+	public void keyPressed(KeyEvent event) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+	}
+}
 
 public class Pong extends JFrame {
 	
+    private final static String WINDOW_TITLE = "Pong";
+    private final static int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+	
 	public Pong() {
-		setTitle("Pong");
-		setSize(800, 600);
-		setResizable(true);
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(new PongPanel());
 	}
 	public static void main(String[] args) {
 		new Pong();
+		//new PongPanel(); doesnt work must be in the Pong constructor window
 	}
 
 }
